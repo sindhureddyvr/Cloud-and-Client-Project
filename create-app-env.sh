77b1ba32a6d1f3224b90268a8801b4d8606dd1fe
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-31 04:53:57 +0000

    Removed an extra space

--- create-app-env.sh
+++ create-app-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Creating MYSQL DB instance"
-aws rds create-db-instance --db-instance-identifier mysqldbinstance --db-instance-class db.m1.small --engine MySQL --master-username masterawsuser --master-user-password masteruserpassword --a
-llocated-storage 20 --backup-retention-period 3
+aws rds create-db-instance --db-instance-identifier mysqldbinstance --db-instance-class db.m1.small --engine MySQL --master-username masterawsuser --master-user-password masteruserpassword --allocated-storage 20 --backup-retention-period 3
 echo “Waiting until the instance is available”
 aws rds wait db-instance-available --db-instance-identifier mysqldbinstance
 echo “MYSQL DB instance created"
