081f07333a02fa308285b90625722d426468ff09
sindhureddyvr <sindhureddyvr@gmail.com>
2016-11-06 23:58:55 -0600

    Moved to install-app-env.sh

--- create-app-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creating MYSQL DB instance"
-aws rds create-db-instance --db-instance-identifier mysqldbinstance --db-instance-class db.m1.small --engine MySQL --master-username masterawsuser --master-user-password masteruserpassword --allocated-storage 20 --backup-retention-period 3
-echo “Waiting until the instance is available”
-aws rds wait db-instance-available --db-instance-identifier mysqldbinstance
-echo “MYSQL DB instance created"
