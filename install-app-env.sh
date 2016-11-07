e663bdb3987a11fe64fba5ee0e9d061db04d6832
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-11-07 04:36:23 +0000

    Creating RDS,SQS,SNS and buckets

---
+++ install-app-env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+parameters=$#
+
+if [ $# -ne 2 ]
+
+then
+
+echo "This script requires 2 bucket names to be passed as raw-svr and raw-uay respectivly.Please pass the right number of parameters and run the script again."
+else
+
+echo "Creating MYSQL DB instance"
+
+aws rds create-db-instance --db-instance-identifier sreddy7 --db-instance-class db.t1.micro --engine mysql --master-username sreddy7 --master-user-password sreddy123 --allocated-storage 20 --db-name school
+
+echo "Waiting until the instance is available"
+
+aws rds wait db-instance-available --db-instance-identifier sreddy7
+
+echo "MYSQL DB instance created"
+
+echo "Creating SNS Topic"
+
+aws sns create-topic --name sreddy7
+
+echo "Creating SQS Queue"
+
+aws sqs create-queue --queue-name sreddy7
+
+echo "Creating a Bucket"
+
+aws s3 mb s3://$1 --region us-west-2
+
+aws s3 mb s3://$2 --region us-west-2
+
+fi
+
