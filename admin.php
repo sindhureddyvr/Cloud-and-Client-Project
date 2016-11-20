2c69a438c4a18c6cb27d7b916d554bf238a6f9a8
sindhureddyvr <sindhureddyvr@gmail.com>
2016-11-19 23:03:30 -0600

    Basic admin.php page

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css">
+    <title>Admin Page</title>
+    <style>
+    body {
+        color: navy !important;;
+        background-color: lightblue !important;;
+        font-family: "Comic Sans MS", cursive, sans-serif ;
+    }
+    </style>
+</head>
+<body>
+<h1>Welcome,Admin </h1>
+</body>
+<html>
