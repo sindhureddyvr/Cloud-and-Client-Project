e86f194a58572bdf7e634a945afc6e24bd7d2d98
sindhureddyvr <sindhureddyvr@gmail.com>
2016-11-20 02:13:34 -0600

    Added CSS and required functionality

--- upload.php
+++ upload.php
@@ -13,6 +13,16 @@
     </style>
 </head>
 <body>
-<h1>Welcome to the Upload </h1>
+<h1>Welcome to the Upload </h1><br/>
+<form enctype="multipart/form-data" action="uploader.php" method="POST">
+    <!-- MAX_FILE_SIZE must precede the file input field -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+
+<input type="file" name="userfile" />
+<input type="submit" value="submit" />
+</form>
+
+
+
 </body>
 <html>
