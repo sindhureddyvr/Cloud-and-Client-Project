a43ac094a5d677effbfa511fc8e5b13808cdab74
sindhureddyvr <sindhureddyvr@gmail.com>
2016-11-19 22:54:31 -0600

    Basic welcome.php file

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css">
+    <title>Welcome Page</title>
+    <style>
+    body {
+        color: navy !important;;
+        background-color: lightblue !important;;
+        font-family: "Comic Sans MS", cursive, sans-serif ;
+    }
+    </style>
+</head>
+<body>
+<h1>Welcome </h1><br/>
+
+<a href="gallery.php"> Gallery</a>
+<a href="upload.php">Upload</a>
+<a href="admin.php">Admin</a><br/><br/><br/>
+<a href="index.php?logout=1" class="btn btn-primary"> Logout</a>
+</body>
+</html>
