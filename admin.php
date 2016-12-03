2aad5d7e576e99c1b3adecd0ca94b4c5982d9b3c
sindhureddyvr <sindhureddyvr@gmail.com>
2016-12-03 05:41:34 -0600

    updated to implement radio buttons to switch on/off upload

--- admin.php
+++ admin.php
@@ -1,39 +1,3 @@
-<?php
-        require 'vendor/autoload.php';
-
-        use Aws\Rds\RdsClient;
-
-        $client = RdsClient::factory(array(
-          'version' => 'latest',
-          'region'  => 'us-west-2'
-        ));
-
-        $result = $client->describeDBInstances(array(
-          'DBInstanceIdentifier' => 'sreddy7',
-        ));
-
-        $endpoint = $result->search('DBInstances[0].Endpoint.Address');
-        
-        $link = mysqli_connect($endpoint,"sreddy7","sreddy123","school") or die("Error " . mysqli_error($link));
-        $result = $link->query('SELECT * FROM items');
-        //Exporting database
-        $fp = fopen('php://output', 'w');
-        if ($fp && $result) {
-                header('Content-Type: text/csv');
-                header('Content-Disposition: attachment; filename="export.csv"');
-
-		$fields = array('id','email','phone','s3rawurl','s3finishedurl','issubscribed','status','receipt');
-                fputcsv($fp,$fields);
-                while ($row = $result->fetch_array(MYSQLI_NUM)) {
-                fputcsv($fp, array_values($row));
-                }
-                die;
-        }
-	$link->close();
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,5 +14,59 @@
 </head>
 <body>
 <h1>Welcome,Admin </h1><br/>
+<form action = "" method = "post">
+Image Upload
+<select name="upload_select_status">
+  <option value="blank"> </option>
+  <option value="On">ON</option>
+  <option value="Off">OFF</option>
+</select>
+<input type="submit" value="Submit" /></br>
+</form>
+<br>
+<br>
+<a href="backup.php"> BackupDB </a>
+<br/><br/>
+<a href="index.php?logout=1" class="btn btn-primary"> Logout </a>
 </body>
 <html>
+
+<?php
+session_start();
+require 'vendor/autoload.php';
+use Aws\Rds\RdsClient;
+$client = RdsClient::factory(array(
+'version' => 'latest',
+'region'  => 'us-west-2'
+));
+$result = $client->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'sreddy7',
+));
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+
+$link = mysqli_connect($endpoint,"sreddy7","sreddy123","school",3306) or die("Error " . mysqli_error($link));
+if (mysqli_connect_errno()) {
+    printf("mysql db Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$upload_status=$_POST["upload_select_status"];
+//echo $upload_status;
+if ( $upload_status == "On" )
+{
+$sql_update_status="update control set upload=1 where id=1";
+}
+elseif ($upload_status == "blank" )
+{
+$sql_update_status="select upload from control where id=1";
+}
+elseif ($upload_status == "Off" )
+{
+$sql_update_status="update control set upload=0 where id=1";
+}
+if ($link->query($sql_update_status) === TRUE) {
+//    echo "Record updated successfully";
+} else {
+    "Error updating record: ";
+}
+?>
