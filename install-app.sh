1eb0dac51a8b7790e948f7dae84747180eb86555
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-12-03 01:40:59 +0000

    Added code to copy all the php files to the /var/www/html

--- install-app.sh
+++ install-app.sh
@@ -3,7 +3,7 @@
 run-one-until-success sudo apt-get update -y
 sleep 30
 echo "Installing apache2,php-xml,php,php-mysql,curl,zip,unzip,git"
-run-one-until-success sudo apt-get install -y apache2 php php-xml php-mysql curl php-curl zip unzip git libapache2-mod-php php7.0-xml php7.0-cli
+run-one-until-success sudo apt-get install -y apache2 php php-xml php-mysql curl php-curl zip unzip git libapache2-mod-php php7.0-xml php7.0-cli php-gd
 echo "Installed apache2,php-xml,php,php-mysql,curl,zip,unzip,git"
 sleep 30
 echo "Setting the environment vairable"
@@ -23,3 +23,9 @@ sudo mv /vendor /var/www/html
 sudo mv /sreddy7/switchonarex.png /var/www/html
 sudo mv /sreddy7/s3test.php /var/www/html
 sudo mv /sreddy7/dbtest.php /var/www/html
+sudo mv /sreddy7/index.php /var/www/html
+sudo mv /sreddy7/gallery.php /var/www/html
+sudo mv /sreddy7/welcome.php /var/www/html
+sudo mv /sreddy7/upload.php /var/www/html
+sudo mv /sreddy7/uploader.php /var/www/html
+
