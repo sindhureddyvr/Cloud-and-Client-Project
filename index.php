a7194f8b38df581e6b0ccb55605195629d199604
sindhureddyvr <sindhureddyvr@gmail.com>
2016-12-02 19:16:24 -0600

    Updated index.php with session

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
@@ -14,11 +14,11 @@ $result = $client->describeDBInstances(array(
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
-print_r($endpoint);
+//print_r($endpoint);
 
-echo "<br/>";
-echo "<br/>";
-echo "begin database";
+//echo "<br/>";
+//echo "<br/>";
+//echo "begin database";
 $link = mysqli_connect($endpoint,"sreddy7","sreddy123","school",3306) or die("Error " . mysqli_error($link));
 
 if (mysqli_connect_errno()) {
@@ -30,10 +30,10 @@ if (mysqli_connect_errno()) {
 $sql = "drop table loginData";
 
 if ($link->query($sql) === TRUE) {
-    echo "<br/>";
-    echo "Dropping loginData table";
+    //echo "<br/>";
+    //echo "Dropping loginData table";
 } else {
-    echo "No table is present to drop";
+    //echo "No table is present to drop";
 }
 
 //Create Table
@@ -45,56 +45,56 @@ $create_table = 'CREATE TABLE IF NOT EXISTS loginData
 )';
 $create_tbl = $link->query($create_table);
 if ($create_table) {
-        echo "<br/>";
-        echo "<b>Table <i>login</i> is created in school database</b>";
-        echo "</br>";
+        //echo "<br/>";
+        //echo "<b>Table <i>login</i> is created in school database</b>";
+        //echo "</br>";
 }
 else {
-        echo "error while creating login table!!";
+      //  echo "error while creating login table!!";
 }
 
 //Insert data
 $sql = "INSERT INTO loginData (username,password) VALUES ('sreddy7@hawk.iit.edu', 'password'), ('jhajek@iit.edu', 'password')";
 
 if ($link->query($sql) === TRUE) {
-    echo "<br/>";
-    echo "<b>2 New records inserted successfully</b>";
+    //echo "<br/>";
+    //echo "<b>2 New records inserted successfully</b>";
 } else {
-    echo "Error while inserting records into login table: " . $sql . "<br>" . $link->error;
+    //echo "Error while inserting records into login table: " . $sql . "<br>" . $link->error;
 }
 
 //Drop table items
 $sql = "drop table items";
 
 if ($link->query($sql) === TRUE) {
-    echo "<br/>";
-    echo "Drop items table if present";
+    //echo "<br/>";
+   // echo "Drop items table if present";
 } else {
-    echo "No table is present to drop";
+    //echo "No table is present to drop";
 }
 
 //Create table items for further use
 $create_table_items = 'CREATE TABLE IF NOT EXISTS items
 (
-    id INT(6) AUTO_INCREMENT,
-    email VARCHAR(20),
-    phone INT(15),
-    filename VARCHAR(30),
-    s3rawurl VARCHAR(30),
-    s3finishedurl VARCHAR(30),
-    status INT(15),
-    issubscribed VARCHAR(10),
-    reciept VARCHAR(30),
+   
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(32),
+    phone VARCHAR(32),
+    s3rawurl VARCHAR(255),
+    s3finishedurl VARCHAR(255),
+    issubscribed INT NOT NULL,
+    status INT,
+    receipt VARCHAR(255),
     PRIMARY KEY(id)
 )';
 $create_tbl = $link->query($create_table_items);
 if ($create_table_items) {
-        echo "<br/>";
-        echo "<b>Table items is created successfully</b>";
-        echo "</br>";
+        //echo "<br/>";
+        //echo "<b>Table items is created successfully</b>";
+        //echo "</br>";
 }
 else {
-        echo "error while creating items table!!";
+      //  echo "error while creating items table!!";
 }
 
 
@@ -111,7 +111,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       //$active = $row['active'];
 
      $count = mysqli_num_rows($result);
-echo $count;
+     //echo $count;
       // If result matched $myusername and $mypassword, table row must be 1 row
 
       if($count > 0) {
