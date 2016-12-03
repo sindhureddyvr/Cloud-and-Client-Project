afce02c4899b07d434a022130098bdcc7ba70f54
sindhureddyvr <sindhureddyvr@gmail.com>
2016-12-03 04:35:00 -0600

    updated admin.php to export data into .csv file

--- admin.php
+++ admin.php
@@ -1,3 +1,39 @@
+<?php
+        require 'vendor/autoload.php';
+
+        use Aws\Rds\RdsClient;
+
+        $client = RdsClient::factory(array(
+          'version' => 'latest',
+          'region'  => 'us-west-2'
+        ));
+
+        $result = $client->describeDBInstances(array(
+          'DBInstanceIdentifier' => 'sreddy7',
+        ));
+
+        $endpoint = $result->search('DBInstances[0].Endpoint.Address');
+        
+        $link = mysqli_connect($endpoint,"sreddy7","sreddy123","school") or die("Error " . mysqli_error($link));
+        $result = $link->query('SELECT * FROM items');
+        //Exporting database
+        $fp = fopen('php://output', 'w');
+        if ($fp && $result) {
+                header('Content-Type: text/csv');
+                header('Content-Disposition: attachment; filename="export.csv"');
+
+		$fields = array('id','email','phone','s3rawurl','s3finishedurl','issubscribed','status','receipt');
+                fputcsv($fp,$fields);
+                while ($row = $result->fetch_array(MYSQLI_NUM)) {
+                fputcsv($fp, array_values($row));
+                }
+                die;
+        }
+	$link->close();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +49,6 @@
     </style>
 </head>
 <body>
-<h1>Welcome,Admin </h1>
+<h1>Welcome,Admin </h1><br/>
 </body>
 <html>
