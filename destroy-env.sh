33e77fdb1d4368eddb57a62e67768f8880998f00
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-12-04 20:10:32 +0000

    Updated destroy to delete cron instance

--- destroy-env.sh
+++ destroy-env.sh
@@ -3,6 +3,8 @@ echo -e "\e[1mLab5 Destroy script\e[0m"
 echo -e "Collect instance id's attached to the auto-scaling-group"
 instance_id=$(aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].[InstanceId]')
 
+#instance_id=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId]' --filters Name=instance-state-name,Values=running)
+
 echo -e "Collect auto-scaling-group-name"
 autoscalingGrpName=$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]')
 
@@ -15,6 +17,10 @@ loadBalancerName=$(aws elb describe-load-balancers --query 'LoadBalancerDescript
 echo -e "\e[1mSet desired capacity to 0 \e[0m"
 aws autoscaling set-desired-capacity --auto-scaling-group-name $autoscalingGrpName --desired-capacity 0
 echo -e "Desired Capacity is set to 0"
+
+croninstance=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId]' --filters Name=instance-state-name,Values=running)
+
+aws ec2 terminate-instances --instance-ids $croninstance
 
 echo -e "\e[1mWait until the instances are terminated\e[0m"
 aws ec2 wait instance-terminated --instance-ids $instance_id
@@ -56,3 +62,8 @@ aws sns delete-topic --topic-arn $ARN
 #delete sqs queue
 URL=`aws sqs get-queue-url --queue-name sreddy7 --query 'QueueUrl' | cut -d\" -f2`
 aws sqs delete-queue --queue-url $URL
+
+croninstance=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId]' --filters Name=instance-state-name,Values=running)
+
+aws ec2 terminate-instances --instance-ids $croninstance
+
